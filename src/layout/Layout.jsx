e9cabfa8a945a88385e264e21b1a2cf16640c946
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-07-01 20:38:02 +0500

    header done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
+import Headertopbar from "../components/Headertopbar";
 
 function Layout() {
   return (
     <div>
+      <Headertopbar></Headertopbar>
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
